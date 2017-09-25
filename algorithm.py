8d31f8ddbc506141a25c9f31f1f1518f595bd0a0
Niki May <nikkimay96@gmail.com>
2017-09-25 12:50:57 -0400

    Created algorithm file
    Comments and function declarations

---
+++ algorithm.py
@@ -0,0 +1,22 @@
+#Connects to server, waits for JSON object
+def connect():
+
+#Reads in a JSON object and stores the data
+#Parse JSON object 
+def readJSON(object):
+
+#Store key, value pairs
+#Read in from DynamoDB to get values of each loc.
+#Returns a multiplier based on the location given
+def findMultiplier(location):
+
+#Creates and populates the 2D array
+#Reads in from DynamoDB to fill row/col 0 with locations
+#Calls calculatePoints() to determine innards of array
+def makeTable():
+
+#Determines points for each location
+def calculatePoints():
+
+#Calls the functions, returns to UI
+def main():
