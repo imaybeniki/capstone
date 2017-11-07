858732a3d8ce26a967d8fd8e70092fc98b2f0482
Niki May <nikkimay96@gmail.com>
2017-11-07 14:57:18 -0500

    got rid of superfluous methods

--- algorithm.py
+++ algorithm.py
@@ -1,34 +1,85 @@
-#Connects to server, waits for JSON object
-def connect():
+import googlemaps
+import math
+#import simplejson, urllib
+from math import radians, cos, sin, asin, sqrt
 
-#Reads in a JSON object and stores the data
-#Parse JSON object 
-def readJSON(object):
+#----------globals------------------------------
+n = 10; #size of the matrix (how many points pulled from Chris)
+map = [[0 for i in range(n)] for j in range(n)]
+nodes = [Node() for i in range(n)]
+data = [0.0 for i in range(3*n)]
+#-------------------------------------------------
 
-#Store key, value pairs
-#Read in from DynamoDB to get values of each loc.
-#Returns a multiplier based on the location given
-def findMultiplier(location):
+class Node(object):
+    id = 0
+    lat = 0
+    lon = 0
+    weight = "Full"
+    
+    def __init__(self, id, lat, lon, weight):
+        self.id = id
+        self.lat = lat
+        self.lon = lon
+        self.weight = weight
 
-#Creates and populates the 2D array
-#Reads in from DynamoDB to fill row/col 0 with locations
-#Calls calculatePoints() to determine innards of array
-def makeTable():
-  #this int will come from the number of entries given by the data base (row and col will be equal)
-  int matrix = 0;
-  #x = [[int for i in range(10)] for j in range(10)]
-
-#Determines points for each location
-#Parameters are tuples of (latitude, longitude) which is the key in the key/value pair
-def calculatePoints(to, from):
-  #find the value for the corresponding to key
-  #multiply the key by the distance between the two points
-  #using a map API? lat, long differences?
-
-#Calls the functions, returns to UI
+ #when calling create node in main, data is pulled from Chris
+def createNode(id, lat, lon, weight):
+    node = Node(id, lat, lon, weight)
+    return node
+        
+    
+def createArray():
+    print("Creating Array")
+    for j in range(n):
+        map[0][j] = nodes[j].id 
+        map[j][0] = nodes[j].id
+        t = t + 1
+        for i in range(n):
+            if(map[i][j] == 0):
+                if(i == j):
+                    map[i][j] = -1
+                else:    
+                    calculation = 3 #calcGoogleBike(transportation)
+                    map[i][j] = calculation
+                    map[j][i] = calculation
+          
+def calculate(orig_lng, orig_lat, dest_lng, dest_lat, threshold):
+    if(threshold == "Full"):
+        return 0
+    else:
+        orig_lng, orig_lat, dest_lng, destlat = map(radians, [orig_lng, orig_lat, dest_lng, destlat])
+        dlon = dest_lng - orig_lng 
+        dlat = dest_lat - orig_lat 
+        a = sin(dlat / 2) ** 2 + cos(orig_lat) * cos(dest_lat) * sin(dlon / 2) ** 2
+        c = 2 * asin(sqrt(a)) 
+        km = 6371 * c
+    
+    
+def calcGoogleBike(transportation, orig_lng, orig_lat, dest_lng, dest_lat, threshold):
+    if(threshold == "Full"):
+        return 0
+    else:
+        orig_coord = orig_lat, orig_lng
+        dest_coord = dest_lat, dest_lng
+        if(transportation == "Bicycle"):
+            urlBike = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=bicycling&language=en-EN&sensor=false".format(str(orig_coord), str(dest_coord))
+            resultBike = simplejson.load(urllib.urlopen(urlBike))
+            biking_time = result['rows'][0]['elements'][0]['duration']['value']
+        if(transportation == "Car"):
+            urlDrive = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(orig_coord), str(dest_coord))
+            resultDrive = simplejson.load(urllib.urlopen(urlDrive))
+            driving_time = result['rows'][0]['elements'][0]['duration']['value']
+            
 def main():
-  #poll every 10 seconds to calculate points
-  #populate the table accordingly
-  #read in from server for JSON object
-  #find the multiplier between the distances
-  #calculate the points and return to the UI
+    i = 0
+    for j in range(n):
+        nodes[j] = createNode(i, data[j], data[j+1], data[j+2])
+        i = i + 1
+        j = j + 3
+    
+    createArray()
+    
+    for j in range(n):
+        for i in range(n):
+            print map[i][j],
+            print('\n')
